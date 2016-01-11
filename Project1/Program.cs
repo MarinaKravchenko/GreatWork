c29c96f25348ef9b56b8a6a8b3d2794d72f9fb35
Mary <myupolyakova_1@edu.hse.ru>
2016-01-11 14:57:57 +0300

    linq

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -8,8 +8,17 @@ namespace Project1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Context context = new Context();
+            FindSongs(context, "Andrew Peters");
+        }
+
+        static Song[] FindSongs(Context context, string SingerName)
+        {
+            var rezult = context.Songs.Where(e => e.Singer.Name == SingerName);
+            return rezult.OrderBy(e => e.Name).ToArray().ToArray();
         }
     }
 }
