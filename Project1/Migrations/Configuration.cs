e0c7038b6268fbe436145864914b3b9cdc053900
Marina <m.v.kravchenko@gmail.com>
2016-01-11 14:52:05 +0300

    Seed Metod Filled In

--- Project1/Migrations/Configuration.cs
+++ Project1/Migrations/Configuration.cs
@@ -14,18 +14,26 @@ namespace Project1.Migrations
 
         protected override void Seed(Project1.Context context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Singers.AddOrUpdate(
+              p => p.Name,
+              new Singer { Name = "Andrew Peters" },
+              new Singer { Name = "Brice Lambson" },
+              new Singer { Name = "Rowan Miller" }
+            );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.SaveChanges();
+
+            context.Songs.AddOrUpdate(
+               p => p.Name,
+               new Song { Name = "Bird", Singer = context.Singers.Single( s => s.Name == "Andrew Peters" ) },
+               new Song { Name = "Wild wind", Singer = context.Singers.Single(s => s.Name == "Brice Lambson") },
+               new Song { Name = "Nature", Singer = context.Singers.Single(s => s.Name == "Rowan Miller") },
+               new Song { Name = "Sounds", Singer = context.Singers.Single( s => s.Name == "Andrew Peters" ) },
+               new Song { Name = "Today", Singer = context.Singers.Single(s => s.Name == "Brice Lambson") },
+               new Song { Name = "Rising sun", Singer = context.Singers.Single(s => s.Name == "Brice Lambson") }
+             );
+
+            context.SaveChanges();
         }
     }
 }
