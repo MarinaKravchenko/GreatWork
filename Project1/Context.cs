c967f47bdc941ca4a4c32686e163db6950505342
Mary <myupolyakova_1@edu.hse.ru>
2016-01-11 14:30:48 +0300

    Migrations Added

---
+++ Project1/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Context : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Singer> Singers { get; set; }
+    }
+}
